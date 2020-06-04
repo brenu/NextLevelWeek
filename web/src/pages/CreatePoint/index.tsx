bc93bfa5959f3bf7c0cce851db0c34279861e811
brenu <bvsousa64@gmail.com>
2020-06-04 15:13:33 -0300

    Alert de sucesso no cadastro - Dia 03

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,6 @@
 import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
-import { FiArrowLeft } from 'react-icons/fi';
+import { FiArrowLeft, FiCheckCircle, } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import { LeafletMouseEvent } from 'leaflet';
 import axios from 'axios';
@@ -35,6 +35,7 @@ interface IBGECityResponse {
 
 
 const CreatePoint = () => {
+    const [ createState, setCreateState ] = useState(false);
     const [ items, setItems ] = useState<Item[]>([]);
     const [ ufs, setUfs ] = useState<string[]>([]);
     const [ cities, setCities ] = useState<string[]>([]);
@@ -149,9 +150,10 @@ const CreatePoint = () => {
 
         await api.post('points', data);
 
-        alert('Ponto de coleta criado!');
-
-        history.push('/');
+        setCreateState(true);
+        window.scrollTo(0,0);
+        
+        setTimeout(() => {history.push('/')}, 2000);
     }
 
     return (
@@ -272,6 +274,13 @@ const CreatePoint = () => {
                     Cadastrar ponto de coleta
                 </button>
             </form>
+
+            { createState === true && (
+                <div className="alert-container">
+                    <FiCheckCircle id="alert-icon" size={72} color="#34CB79" />
+                    <p className="alert-text">Cadastro concluído!</p>
+                </div>
+            )}
         </div>
     );
 }
